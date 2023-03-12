542e99ee32ed78b83a868fec254182e7a0a98559
KeftonDavid <keftondavid@gmail.com>
2023-03-12 01:56:34 -0300

    Evento 'hover' funcionando de acordo com o posicionamento da ponta do dedo indicador

--- classes/class04/pages/titles/src/factories/handGestureFactory.js
+++ classes/class04/pages/titles/src/factories/handGestureFactory.js
@@ -10,6 +10,7 @@ import HandGestureView from "../views/handGestureView.js"
 import Camera from "../../../../lib/shared/camera.js"
 import { fingerLookupIndexes, gestureStrings, knownGestures } from "../util/util.js"
 
+const styler = new PseudoStyler()
 const camera = await Camera.init()
 
 const factory = {
@@ -17,7 +18,8 @@ const factory = {
     return HandGestureController.initialize({
       camera,
       view: new HandGestureView({
-        fingerLookupIndexes
+        fingerLookupIndexes,
+        styler
       }),
       service: new HandGestureService({
         gestureStrings,
